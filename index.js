f8b9f4f7b766ffd86075b57089cd6d7df49a37ed
Simon Renoult <simon.renoult@gmail.com>
2018-03-26 05:32:50 +0200

    feat: count commits

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code goes here.
+const { exec } = require("child_process");
+const { dirname, basename } = require("path");
+const { promisify } = require("util");
+
+module.exports = {
+  countCommits
+};
+
+async function countCommits(fileLocation) {
+  const directory = dirname(fileLocation);
+  const file = basename(fileLocation);
+
+  const { stdout, stderr } = await promisify(exec)(
+    `git -C ${directory} log --oneline --first-parent ${file} | wc -l`
+  );
+
+  if (stderr) throw stderr;
+
+  return stdout.trim();
+}
